ee5b338fd3bc9fbbfd532795371314cfbb4b3b7a
Amey Shinde <ameyshinde11111@gmail.com>
2024-02-26 19:18:44 -0600

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -5,7 +5,7 @@ import Navbar from "./Navbar";
 import LeftSidebar from "./LeftSidebar";
 import RightSidebar from "./RightSidebar";
 import CursorChat from "./cursor/CursorChat";
-import Live from "./Live"
+import Live from "./Live";
 
 export {
   LiveCursors,
